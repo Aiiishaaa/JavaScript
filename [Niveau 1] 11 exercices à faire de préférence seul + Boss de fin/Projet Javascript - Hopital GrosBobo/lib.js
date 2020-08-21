516bf8de2e051c4c050bbc76a080c082b3bf64dd
Aiiishaaa <aicha.hamida06@yahoo.fr>
2020-08-21 16:58:39 +0200

    2nd commit

--- [Niveau 1] 11 exercices à faire de préférence seul + Boss de fin/Projet Javascript - Hopital GrosBobo/lib.js
+++ [Niveau 1] 11 exercices à faire de préférence seul + Boss de fin/Projet Javascript - Hopital GrosBobo/lib.js
@@ -1,5 +1,12 @@
-function showHxContent(pNumber) {
-    vDiv = document.getElementById("title" + pNumber);
-    vDiv.style.display = "block";
-    document.getElementsByTagName.get
+function showContent(pNumber) {
+    deleteAllP(pNumber);
+    var p = document.getElementById('title' + pNumber)
+    p.style.display = "block";
+}
+
+function deleteAllP(pNumber) {
+    hideP = document.getElementsByTagName("p");
+    for (var i = 0; i < hideP.length; i++) {
+        hideP.item(i).style.display = "none";
+    }
 }
